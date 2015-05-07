c6c1ca6e2c6d2c259a9251f58ee9ed2795040d65
Andy Sweet <andrew.d.sweet@gmail.com>
2015-05-06 20:18:09 -0700

    changed napoleon import for sphinx 1.3

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 ]
